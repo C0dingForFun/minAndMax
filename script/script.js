c4c4a3c4733874227ff69e4a63ee5980f773b9de
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-06-04 15:55:17 +0200

    minNmax

--- script/script.js
+++ script/script.js
@@ -5,7 +5,8 @@ let max = document.querySelectorAll('span')[1];
 
 button.addEventListener('click', ()=>{
 
-    let num = numbers.value.split(',');
+    let num = numbers.value.split(',');//store all values into "num" and converts it into an array
+
     min.innerText = Math.min(...num);
     max.innerText = Math.max(...num);
 
